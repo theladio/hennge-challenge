458b660e682f5eade23266c402610891b08f61e2
Björn Leijdström <bjorn.leijdstrom@advisa.se>
2020-11-05 23:08:33 +0100

    Add mail-list component
    Setup the empty state, default design for 0 mails.

---
+++ src/js/components/mail-list/mail-list.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Logo from '../../../assets/logo.png';
+
+import './mail-list.scss';
+
+const MailList = () => (
+  <div className="mail-list">
+    <img src={Logo} alt="Mail Archiver" />
+  </div>
+);
+
+export default MailList;
