dd94f3db055ceedd8c42b8058e3191b42d19e469
The LAD <hello@thelad.io>
2020-11-05 14:25:24 +0100

    Add styling to date-picker
    Make sure our date-picker looks like the sketch provided by HENNGE.

--- src/js/components/date-picker/date-picker.js
+++ src/js/components/date-picker/date-picker.js
@@ -1,24 +1,45 @@
 import React, { useState } from 'react';
 
+import './date-picker.scss';
+
 const DatePicker = () => {
   const [startDate, setStartDate] = useState('');
   const [endDate, setEndDate] = useState('');
 
+  const setDate = (set) => (event) => set(event.target.value);
+  const searchSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <div className="date-picker">
-      <input
-        type="date"
-        value={startDate}
-        onChange={e => setStartDate(e.target.value)}
-      />
-      -
-      <input
-        type="date"
+    <label
+      className="date-picker"
+      htmlFor="startDate"
+    >
+      <div className="date-picker__group">
+        <input
+          className="date-picker__input"
+          id="startDate"
+          type="date"
+          value={startDate}
+          onChange={setDate(setStartDate)}
+        />
+        -
+        <input
+          className="date-picker__input"
+          type="date"
+          value={endDate}
+          onChange={setDate(setEndDate)}
+        />
+      </div>
+      <button
+        className="date-picker__submit"
+        type="submit"
         value={endDate}
-        onChange={e => setEndDate(e.target.value)}
+        onClick={searchSubmit}
       />
-    </div>
+    </label>
   );
-}
+};
 
 export default DatePicker;
