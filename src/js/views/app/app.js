c3f642e77e4054d92dc8f66087682e6470a9735d
Björn Leijdström <bjorn.leijdstrom@advisa.se>
2020-11-05 22:47:54 +0100

    Add margins to components
    Just give our components in the `app`-view some space to match sketch
    from HENNGE.

--- src/js/views/app/app.js
+++ src/js/views/app/app.js
@@ -5,14 +5,15 @@ import Counter from '../../components/counter';
 import DatePicker from '../../components/date-picker';
 
 import '../../../css/all.scss';
+import './app.scss';
 
 const App = () => (
-  <>
-    <form>
+  <div className="app">
+    <form className="app__form">
       <DatePicker />
     </form>
     <Counter />
-  </>
+  </div>
 );
 
 export default App;
