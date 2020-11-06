0959655f63f5359cf6afd77ac8f7e373b7c583fe
The LAD <hello@thelad.io>
2020-11-06 22:38:04 +0100

    Add sorting-row for mail-list
    Make sure it at least looks like you can sort the mail-items.

--- src/js/components/mail-list/mail-list.js
+++ src/js/components/mail-list/mail-list.js
@@ -1,25 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import MailRow from '../mail-row';
 import Logo from '../../../assets/logo.png';
 
 import './mail-list.scss';
 
-const MailList = ({ mails = [] }) => (
-  <div className={
-    `mail-list mail-list--${!!mails.length}`
-  }>
-    {mails.length
-      ? mails.map((item, key) => (
-        <MailRow
-          key={`mail-${key}`}
-          mail={item}
-        />
-      )) : (
-        <img src={Logo} alt="Mail Archiver" />
-      )
+const sortList = [
+  'from',
+  'to',
+  'subject',
+  'date',
+];
+
+const MailList = ({ mails = [] }) => {
+  const [sortBy, setSortBy] = useState('date');
+  const [sortOrder, setSortOrder] = useState('asc');
+
+  const sortMails = (type) => {
+    if (type === sortBy) { setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc'); }
+    else {
+      setSortOrder('asc');
+      setSortBy(type);
     }
-  </div>
-);
+  };
+
+  return (
+    <div className={
+      `mail-list mail-list--${!!mails.length}`
+    }>
+      {mails.length
+        ? (
+          <>
+            <div className="mail-list__sort">
+              {sortList.map((type, key) => (
+                <button
+                  className="mail-list__sort-option"
+                  key={`sort-${key}`}
+                  type="button"
+                  onClick={() => sortMails(type)}
+                >
+                  {type}
+                  {sortBy === type && (
+                    <span className="mail-list__sort-option-selected">
+                      {sortOrder === 'asc' ? '▲' : '▼'}
+                    </span>
+                  )}
+                </button>
+              ))}
+            </div>
+
+            {mails.map((item, key) => (
+              <MailRow
+                key={`mail-${key}`}
+                mail={item}
+              />
+            ))}
+          </>
+        ) : (
+          <img src={Logo} alt="Mail Archiver" />
+        )
+      }
+    </div>
+  );
+};
 
 export default MailList;
