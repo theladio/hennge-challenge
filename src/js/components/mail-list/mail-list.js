76bf6f8e3f3a129e2a955c95b711f7e3856a843e
Björn Leijdström <bjorn.leijdstrom@advisa.se>
2020-11-06 08:54:21 +0100

    Support array of mails in mail-list
    Our `mail-list` needs to support showing mails and not only an empty
    state. Also add a dummy-component for each `mail-row`.

--- src/js/components/mail-list/mail-list.js
+++ src/js/components/mail-list/mail-list.js
@@ -1,12 +1,24 @@
 import React from 'react';
 
+import MailRow from '../mail-row';
 import Logo from '../../../assets/logo.png';
 
 import './mail-list.scss';
 
-const MailList = () => (
-  <div className="mail-list">
-    <img src={Logo} alt="Mail Archiver" />
+const MailList = ({ mails = [] }) => (
+  <div className={
+    `mail-list mail-list--${!!mails.length}`
+  }>
+    {mails.length
+      ? mails.map((item, key) => (
+        <MailRow
+          key={`mail-${key}`}
+          mail={item}
+        />
+      )) : (
+        <img src={Logo} alt="Mail Archiver" />
+      )
+    }
   </div>
 );
 
