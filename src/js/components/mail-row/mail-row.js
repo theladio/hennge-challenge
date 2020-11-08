a20672cce025ed5d559120993d8db97b0bab554d
Björn Leijdström <bjorn.leijdstrom@advisa.se>
2020-11-08 13:55:03 +0100

    Make mail-row responsive
    Make sure we can read the `mail-list` great on mobile as well.

--- src/js/components/mail-row/mail-row.js
+++ src/js/components/mail-row/mail-row.js
@@ -4,16 +4,16 @@ import './mail-row.scss';
 
 const MailRow = ({ mail = {} }) => (
   <div className="mail-row">
-    <div className="mail-row__data">
+    <div className="mail-row__data mail-row__data--from">
       {mail.from}
     </div>
-    <div className="mail-row__data">
+    <div className="mail-row__data mail-row__data--to">
       {mail.to}
     </div>
-    <div className="mail-row__data">
+    <div className="mail-row__data mail-row__data--subject">
       {mail.subject}
     </div>
-    <div className="mail-row__data">
+    <div className="mail-row__data mail-row__data--date">
       {mail.date}
     </div>
   </div>
